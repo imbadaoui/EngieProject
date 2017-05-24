091fbe1f65ca96fefccf9d33ed305c33d7a58ea5
Imad BADAOUI <i.badaoui@MacBook-Pro-de-SQLI.local>
2017-05-24 11:08:53 +0200

    Initial Commit

---
+++ EngieSmartProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EngieSmartProject
+//
+//  Created by Imad BADAOUI on 24/05/2017.
+//  Copyright © 2017 Imad BADAOUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EngieSmartProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EngieSmartProject
+//
+//  Created by Imad BADAOUI on 24/05/2017.
+//  Copyright © 2017 Imad BADAOUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
